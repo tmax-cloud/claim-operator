96ec97f15d0127bff8fb9149814e0a97dea94f26
chosangwon93 <sangwon_cho@tmax.co.kr>
2021-02-18 10:58:06 +0900

    test

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-img=tmaxcloudck/claim-operator:b5.0.2.8
+img=tmaxcloudck/claim-operator:b5.0.4.0
 docker rmi $img
-make docker-build docker-push IMG=$img
+make docker-build deploy IMG=$img
