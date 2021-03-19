e215f5c34fb8660bd9cfb47a5d1ce8605f757f8c
chosangwon93 <sangwon_cho@tmax.co.kr>
2021-03-19 15:58:43 +0900

    copy.sh

--- copy.sh
+++ copy.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-scp test-bycho -r /root/git/5.0/claim-operator root@220.90.208.100:/root/hypercloud5.0/
+scp test-bycho -r /root/git/5.0/claim-operator root@220.90.208.100:/root/hypercloud5.0222
